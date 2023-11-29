645b39430a158a507dda6e21f312a56fc9cb8abf
Karsten Klein <karsten.klein@metaeffekt.com>
2023-11-29 15:24:20 +0100

    Adding package-lock parsing support; fixing missing open code status issue; supporting non-official osi status

---
+++ libraries/ae-inventory-processor/src/main/java/org/metaeffekt/core/inventory/processor/adapter/NpmPackageLockAdapter.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2009-2022 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.metaeffekt.core.inventory.processor.adapter;
+
+import org.json.JSONObject;
+import org.metaeffekt.core.inventory.processor.model.Artifact;
+import org.metaeffekt.core.inventory.processor.model.Constants;
+import org.metaeffekt.core.inventory.processor.model.Inventory;
+import org.metaeffekt.core.util.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Extracts an inventory for production npm modules based on a package-lock.json file.
+ */
+public class NpmPackageLockAdapter {
+
+    /**
+     *
+     * @param packageLockJsonFile The package-lock.json file to parse.
+     * @param relPath The relative path to the file from the relevant basedir.
+     *
+     * @return An inventory popoulated with the runtime modules defined in the package json file.
+     *
+     * @throws IOException
+     */
+    public Inventory createInventoryFromPackageLock(File packageLockJsonFile, String relPath) throws IOException {
+        final Inventory inventory = new Inventory();
+
+        populateInventory(packageLockJsonFile, inventory, relPath);
+
+        return inventory;
+    }
+
+    private void populateInventory(File packageLockJsonFile, Inventory inventory, String path) throws IOException {
+        final String json = FileUtils.readFileToString(packageLockJsonFile, FileUtils.ENCODING_UTF_8);
+        JSONObject obj = new JSONObject(json);
+        addDependencies(obj, inventory, path, "dependencies");
+        addDependencies(obj, inventory, path, "peerDependencies");
+    }
+
+    private void addDependencies(JSONObject obj, Inventory inventory, String path, String dependencyTag) {
+        if (!obj.has(dependencyTag)) return;
+        JSONObject dependencies = obj.getJSONObject(dependencyTag);
+        for (String key : dependencies.keySet()) {
+            JSONObject dep = dependencies.getJSONObject(key);
+
+            String version = dep.getString("version");
+            String url = dep.has("resolved") ? dep.getString("resolved") : null;
+
+            Artifact artifact = new Artifact();
+
+            artifact.setId(key + "-" + version);
+            artifact.setComponent(key);
+            artifact.setVersion(version);
+            artifact.set(Constants.KEY_TYPE, Constants.ARTIFACT_TYPE_NODEJS_MODULE);
+            artifact.setUrl(url);
+            artifact.set(Constants.KEY_PATH_IN_ASSET, path);
+
+            boolean production = !dep.has("dev") || !dep.getBoolean("dev");
+
+            // only consider production artifacts
+            if (production) {
+                inventory.getArtifacts().add(artifact);
+
+                addDependencies(dep, inventory, path, dependencyTag);
+            }
+        }
+    }
+
+}
