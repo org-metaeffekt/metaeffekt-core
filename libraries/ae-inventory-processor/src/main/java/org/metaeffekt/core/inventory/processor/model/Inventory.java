b27e1c3e2dfa0ca20c8f214081035c3f870f66a8
Karsten Klein <karsten.klein@metaeffekt.com>
2024-05-10 09:23:44 +0200

    fix: backward compatibility

--- libraries/ae-inventory-processor/src/main/java/org/metaeffekt/core/inventory/processor/model/Inventory.java
+++ libraries/ae-inventory-processor/src/main/java/org/metaeffekt/core/inventory/processor/model/Inventory.java
@@ -852,6 +852,10 @@ public class Inventory implements Serializable {
         if (StringUtils.isEmpty(type)) return true;
         if (ARTIFACT_TYPE_PACKAGE.equalsIgnoreCase(type)) return false;
         if (ARTIFACT_TYPE_NODEJS_MODULE.equalsIgnoreCase(type)) return false;
+
+        // backward compatibility; constant about to be changes/consolidated
+        if ("nodejs-module".equalsIgnoreCase(type)) return false;
+
         return true;
     }
 
@@ -859,6 +863,10 @@ public class Inventory implements Serializable {
         String type = a.get(KEY_TYPE);
         if (StringUtils.isEmpty(type)) return false;
         if (ARTIFACT_TYPE_NODEJS_MODULE.equalsIgnoreCase(type)) return true;
+
+        // backward compatibility; constant about to be changes/consolidated
+        if ("nodejs-module".equalsIgnoreCase(type)) return true;
+
         return false;
     }
 
