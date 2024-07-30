e0f55319512e95bea2466e668acdf87e3df28522
Ben Klein <ben.klein@metaeffekt.com>
2024-07-30 11:52:36 +0200

    tests: added new tests for cocoapods, flutter pubs anf flutter platform

---
+++ tests/ae-inventory-subjects-itest/src/test/java/org/metaeffekt/core/itest/analysis/libraries/ffmpeg_kit_flutter_6_0_3.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2009-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.metaeffekt.core.itest.analysis.libraries;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.metaeffekt.core.itest.common.setup.AbstractCompositionAnalysisTest;
+import org.metaeffekt.core.itest.common.setup.UrlBasedTestSetup;
+
+import static org.metaeffekt.core.inventory.processor.model.Artifact.Attribute.*;
+import static org.metaeffekt.core.itest.common.predicates.AttributeValue.attributeValue;
+
+public class ffmpeg_kit_flutter_6_0_3 extends AbstractCompositionAnalysisTest {
+
+    @BeforeClass
+    public static void prepare() {
+        testSetup = new UrlBasedTestSetup()
+                .setSource("https://pub.dev/api/archives/ffmpeg_kit_flutter-6.0.3.tar.gz")
+                .setSha256Hash("843aae41823ca94a0988d975b4b6cdc6948744b9b7e2707d81a3a9cd237b0100")
+                .setName(ffmpeg_kit_flutter_6_0_3.class.getName());
+    }
+
+
+    @Ignore
+    @Test
+    public void clear() throws Exception {
+        Assert.assertTrue(testSetup.clear());
+    }
+
+    @Ignore
+    @Test
+    public void inventorize() throws Exception {
+        Assert.assertTrue(testSetup.rebuildInventory());
+    }
+
+    @Test
+    public void first() throws Exception {
+        getAnalysisAfterInvariantCheck()
+                .selectArtifacts()
+                .logListWithAllAttributes()
+                .with(attributeValue(ID, "ffmpeg_kit_flutter-6.0.3"),
+                        attributeValue(VERSION, "6.0.3"),
+                        attributeValue(VIRTUAL_ROOT_PATH, "[ffmpeg_kit_flutter-6.0.3.tar.gz]/[ffmpeg_kit_flutter-6.0.3.tar]"),
+                        attributeValue(PURL, "pkg:pub/ffmpeg_kit_flutter@6.0.3"),
+                        attributeValue(COMPONENT_SOURCE_TYPE, "pub")
+                )
+                .assertNotEmpty();
+    }
+
+}
---
+++ tests/ae-inventory-subjects-itest/src/test/java/org/metaeffekt/core/itest/analysis/platforms/Flutter_1_16_3.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2009-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.metaeffekt.core.itest.analysis.platforms;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.metaeffekt.core.itest.common.fluent.ArtifactList;
+import org.metaeffekt.core.itest.common.setup.AbstractCompositionAnalysisTest;
+import org.metaeffekt.core.itest.common.setup.UrlBasedTestSetup;
+
+import static org.metaeffekt.core.inventory.processor.model.Artifact.Attribute.*;
+import static org.metaeffekt.core.inventory.processor.model.Artifact.Attribute.PATH_IN_ASSET;
+import static org.metaeffekt.core.itest.common.predicates.AttributeValue.attributeValue;
+
+public class Flutter_1_16_3 extends AbstractCompositionAnalysisTest{
+
+    @BeforeClass
+    public static void prepare() {
+        testSetup = new UrlBasedTestSetup()
+                .setSource("https://github.com/flutter/flutter/archive/refs/tags/v1.16.3.zip")
+                .setSha256Hash("1c965bee13fe7be931ecb9298031e2c0cf1760788047c00ffb3f37c74b9a73d2")
+                .setName(Flutter_1_16_3.class.getName());
+    }
+
+
+    @Ignore
+    @Test
+    public void clear() throws Exception {
+        Assert.assertTrue(testSetup.clear());
+    }
+
+    @Ignore
+    @Test
+    public void inventorize() throws Exception {
+        Assert.assertTrue(testSetup.rebuildInventory());
+    }
+
+    @Test
+    public void assertContent() throws Exception {
+        ArtifactList artifactList = getAnalysisAfterInvariantCheck()
+                .selectArtifacts()
+                .filter(a -> a.getVersion() != null);
+
+        artifactList.logListWithAllAttributes();
+
+        artifactList.with(attributeValue(ID, "snippets-0.1.0"),
+                    attributeValue(VERSION, "0.1.0"),
+                    attributeValue(PROJECTS, "[v1.16.3.zip]/flutter-1.16.3/dev/snippets"),
+                    attributeValue(PATH_IN_ASSET, "[v1.16.3.zip]/flutter-1.16.3/dev/snippets"))
+                .assertNotEmpty();
+
+        artifactList.with(attributeValue(ID, "ios_add2app_flutter-1.0.0+1"),
+                    attributeValue(VERSION, "1.0.0+1"),
+                    attributeValue(PROJECTS, "[v1.16.3.zip]/flutter-1.16.3/dev/integration_tests/ios_add2app/flutterapp"),
+                    attributeValue(PATH_IN_ASSET, "[v1.16.3.zip]/flutter-1.16.3/dev/integration_tests/ios_add2app/flutterapp"))
+                .assertNotEmpty();
+
+        artifactList.with(attributeValue(ID, "splash_screen_load_rotate-1.0.0+1"),
+                    attributeValue(VERSION, "1.0.0+1"),
+                    attributeValue(PROJECTS, "[v1.16.3.zip]/flutter-1.16.3/dev/integration_tests/android_splash_screens/splash_screen_load_rotate"),
+                    attributeValue(PATH_IN_ASSET, "[v1.16.3.zip]/flutter-1.16.3/dev/integration_tests/android_splash_screens/splash_screen_load_rotate"))
+                .assertNotEmpty();
+
+        artifactList.with(attributeValue(ID, "vitool-0.0.1"),
+                    attributeValue(VERSION, "0.0.1"),
+                    attributeValue(PROJECTS, "[v1.16.3.zip]/flutter-1.16.3/dev/tools/vitool"),
+                    attributeValue(PATH_IN_ASSET, "[v1.16.3.zip]/flutter-1.16.3/dev/tools/vitool"))
+                .assertNotEmpty();
+
+        artifactList.with(attributeValue(ID, "image_list-1.0.0+1"),
+                    attributeValue(VERSION, "1.0.0+1"),
+                    attributeValue(PROJECTS, "[v1.16.3.zip]/flutter-1.16.3/examples/image_list"),
+                    attributeValue(PATH_IN_ASSET, "[v1.16.3.zip]/flutter-1.16.3/examples/image_list"))
+                .assertNotEmpty();
+    }
+
+}
