9875652962c99768143ab48881ae4d53c0257510
Ben Klein <ben.klein@metaeffekt.com>
2024-08-08 12:51:34 +0200

    tests: added new tests to gems

---
+++ tests/ae-inventory-subjects-itest/src/test/java/org/metaeffekt/core/itest/analysis/gems/Uglifier_4_2_0.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2009-2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.metaeffekt.core.itest.analysis.gems;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.metaeffekt.core.itest.common.fluent.ArtifactList;
+import org.metaeffekt.core.itest.common.setup.AbstractCompositionAnalysisTest;
+import org.metaeffekt.core.itest.common.setup.UrlBasedTestSetup;
+
+import static org.metaeffekt.core.inventory.processor.model.Artifact.Attribute.*;
+import static org.metaeffekt.core.itest.common.predicates.AttributeValue.attributeValue;
+
+public class Uglifier_4_2_0 extends AbstractCompositionAnalysisTest{
+
+    @BeforeClass
+    public static void prepare() {
+        testSetup = new UrlBasedTestSetup()
+                .setSource("https://rubygems.org/downloads/uglifier-4.2.0.gem")
+                .setSha256Hash("ae68f286bf78a23938f8ff2b8d3cab4aa3d65228c1006476da7023e3ff3af071")
+                .setName(Uglifier_4_2_0.class.getName());
+    }
+
+
+    @Ignore
+    @Test
+    public void clear() throws Exception {
+        Assert.assertTrue(testSetup.clear());
+    }
+
+    @Ignore
+    @Test
+    public void inventorize() throws Exception {
+        Assert.assertTrue(testSetup.rebuildInventory());
+    }
+
+    @Test
+    public void assertContent() throws Exception {
+        ArtifactList artifactList = getAnalysisAfterInvariantCheck()
+                .selectArtifacts();
+
+        artifactList.logListWithAllAttributes();
+
+        artifactList.with(attributeValue(ID, "uglifier-4.2.0.gem"),
+                        attributeValue(VERSION, "4.2.0"),
+                        attributeValue(PROJECTS, "uglifier-4.2.0.gem"))
+                .assertNotEmpty();
+    }
+}
