feef546a255da41bfdf09fc12a6c6cb4699d250a
Thomas Martin <thomas.martin@metaeffekt.com>
2024-09-30 13:08:54 +0200

    initial commit
    Signed-off-by: Thomas Martin <tmartin@MBP-102022.fritz.box>

---
+++ libraries/ae-inventory-processor/src/main/java/org/metaeffekt/core/inventory/processor/report/model/aeaa/csaf/AeaaCsafAdvisoryEntry.java
@@ -0,0 +1,12 @@
+package org.metaeffekt.core.inventory.processor.report.model.aeaa.csaf;
+
+import org.metaeffekt.core.inventory.processor.report.model.aeaa.advisory.AeaaAdvisoryEntry;
+import org.metaeffekt.core.inventory.processor.report.model.aeaa.store.AeaaAdvisoryTypeIdentifier;
+
+public class AeaaCsafAdvisoryEntry extends AeaaAdvisoryEntry {
+
+
+    public AeaaCsafAdvisoryEntry(AeaaAdvisoryTypeIdentifier<?> source) {
+        super(source);
+    }
+}
