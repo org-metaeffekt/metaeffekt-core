7328c3b04fe06601abb67c3e63d07613452c4e21
romeotueremis <romeo.tueremis@mmetaeffekt.com>
2024-11-04 13:34:26 +0100

    style: updated class with lombok Getter/setter

--- libraries/ae-inventory-processor/src/main/java/org/metaeffekt/core/inventory/processor/report/ReportContext.java
+++ libraries/ae-inventory-processor/src/main/java/org/metaeffekt/core/inventory/processor/report/ReportContext.java
@@ -15,46 +15,23 @@
  */
 package org.metaeffekt.core.inventory.processor.report;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.apache.commons.lang3.StringUtils;
 
 import java.util.Map;
 
+@Getter
+@Setter
 public class ReportContext {
 
     private String id;
     private String title;
     private String context;
 
-    public ReportContext() {
-    }
-
     public ReportContext(String id, String title, String context) {
         this.id = id;
         this.title = title;
-        this.context = context;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContext() {
-        return context;
-    }
-
-    public void setContext(String context) {
         this.context = context;
     }
 
