3d2599b76ac856e726d46be22dc2adbc484b9780
romeotueremis <romeo.tueremis@metaeffekt.com>
2025-04-30 13:01:55 +0200

    feat: removed "omitAssetPrefix"

--- libraries/ae-inventory-processor/src/main/java/org/metaeffekt/core/document/report/DocumentDescriptorReportGenerator.java
+++ libraries/ae-inventory-processor/src/main/java/org/metaeffekt/core/document/report/DocumentDescriptorReportGenerator.java
@@ -200,18 +200,7 @@ public class DocumentDescriptorReportGenerator {
                     report.setTargetComponentDir(new File(mergedParams.get("targetComponentDir")));
                 }
 
-                boolean omitAssetPrefix = Boolean.parseBoolean(mergedParams.get("omitAssetPrefix"));
-                String title = null;
-
-                if (mergedParams.get("omitAssetPrefix") != null) {
-                    if (!omitAssetPrefix) {
-                        title = inventoryContext.getReportContextTitle();
-                    }
-                } else {
-                    title = inventoryContext.getReportContextTitle();
-                }
-
-                report.setReportContext(new ReportContext(inventoryContext.getIdentifier(), title, inventoryContext.getReportContext()));
+                report.setReportContext(new ReportContext(inventoryContext.getIdentifier(), inventoryContext.getReportContextTitle(), inventoryContext.getReportContext()));
 
                 report.getReportContext().setReportInventoryName(inventoryContext.getReportContextTitle());
 
