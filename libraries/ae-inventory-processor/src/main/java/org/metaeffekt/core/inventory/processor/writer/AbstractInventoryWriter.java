487c9a4b6a70c47d28194a110153d51bfb27d194
karsten-klein <karsten.klein@metaeffekt.com>
2025-09-05 19:25:25 +0200

    AE-1315: refactoring core
    - moved reinsert method up the hierarchy

--- libraries/ae-inventory-processor/src/main/java/org/metaeffekt/core/inventory/processor/writer/AbstractInventoryWriter.java
+++ libraries/ae-inventory-processor/src/main/java/org/metaeffekt/core/inventory/processor/writer/AbstractInventoryWriter.java
@@ -48,6 +48,15 @@ public abstract class AbstractInventoryWriter {
         }
     }
 
+    protected int reinsert(int insertIndex, String key, List<String> orderedAttributesList, Set<String> attributesSet) {
+        if (attributesSet.contains(key)) {
+            orderedAttributesList.remove(key);
+            orderedAttributesList.add(Math.min(insertIndex, orderedAttributesList.size()), key);
+            insertIndex++;
+        }
+        return insertIndex;
+    }
+
     // FIXME: must be moved more centrally; InventoryUtils?
     protected static boolean isAssetId(String key) {
         return key.startsWith("CID-") ||
