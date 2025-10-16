5dce7835d0cc90bd18eaf3be8176715feae2acab
ywittmann <yan.wittmann@metaeffekt.com>
2025-10-16 12:31:11 +0200

    AE-1373: Priority Score Refactor initiated: CVSS and keyword scores now provide their own scaling functions; removed concept of "isElevated" and instead assign a label to all vulnerabilities no matter what; introduced "none" severity category for priority score; applied changes to report to support this
    Signed-off-by: ywittmann <yan.wittmann@metaeffekt.com>

--- libraries/ae-security/src/main/java/org/metaeffekt/core/security/cvss/CvssSeverityRanges.java
+++ libraries/ae-security/src/main/java/org/metaeffekt/core/security/cvss/CvssSeverityRanges.java
@@ -199,7 +199,8 @@ public class CvssSeverityRanges {
     public static final CvssSeverityRanges PRIORITY_SCORE_SEVERITY_RANGES = new CvssSeverityRanges(
             "escalate:strong-red:9.0:," +
                     "due:strong-dark-orange:7.0:8.9," +
-                    "elevated:strong-light-orange::6.9"
+                    "elevated:strong-light-orange:4.0:6.9," +
+                    "none:pastel-gray::3.9"
     );
     public static final CvssSeverityRanges EPSS_SCORE_SEVERITY_RANGES = new CvssSeverityRanges(
             ">0.8:strong-red:0.8:," +
