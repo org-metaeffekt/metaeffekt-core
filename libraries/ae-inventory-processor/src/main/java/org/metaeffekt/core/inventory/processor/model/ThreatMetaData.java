cb86ddf9a5a11f9b67abaa0d2909fab9d4ffa113
Thomas Martin <thomas.martin@metaeffekt.com>
2026-01-29 14:26:29 +0100

    AE-1736: updated to display rows of threat in inventory
    Signed-off-by: Thomas Martin <thomas.martin@metaeffekt.com>

--- libraries/ae-inventory-processor/src/main/java/org/metaeffekt/core/inventory/processor/model/ThreatMetaData.java
+++ libraries/ae-inventory-processor/src/main/java/org/metaeffekt/core/inventory/processor/model/ThreatMetaData.java
@@ -36,8 +36,8 @@ public class ThreatMetaData extends AbstractModelBase{
         SOURCE("Source"),
         RATIONALE("Rationale"),
 
-        BASED_ON("Based On Threat Reference"),
-        RELATED_TO("Related To Threat Reference"),
+        BASED_ON("'Based On' Threat Reference"),
+        RELATED_TO("'Related To' Threat Reference"),
         THREAT_REFERENCES("Threat References"),
 
         IMPACT_ASSESSMENTS("Impact Assessments"),
@@ -80,6 +80,21 @@ public class ThreatMetaData extends AbstractModelBase{
     public static ArrayList<String> CORE_ATTRIBUTES = new ArrayList<>();
     static {
         CORE_ATTRIBUTES.add(ThreatMetaData.Attribute.ID.getKey());
+        CORE_ATTRIBUTES.add(ThreatMetaData.Attribute.NAME.getKey());
+
+        CORE_ATTRIBUTES.add(ThreatMetaData.Attribute.DESCRIPTION.getKey());
+        CORE_ATTRIBUTES.add(ThreatMetaData.Attribute.SOURCE.getKey());
+        CORE_ATTRIBUTES.add(ThreatMetaData.Attribute.RATIONALE.getKey());
+
+        CORE_ATTRIBUTES.add(ThreatMetaData.Attribute.BASED_ON.getKey());
+        CORE_ATTRIBUTES.add(ThreatMetaData.Attribute.RELATED_TO.getKey());
+        CORE_ATTRIBUTES.add(ThreatMetaData.Attribute.THREAT_REFERENCES.getKey());
+
+        CORE_ATTRIBUTES.add(ThreatMetaData.Attribute.IMPACT_ASSESSMENTS.getKey());
+
+        CORE_ATTRIBUTES.add(ThreatMetaData.Attribute.REFERENCED_VULNERABILITIES.getKey());
+        CORE_ATTRIBUTES.add(ThreatMetaData.Attribute.REFERENCED_SECURITY_ADVISORIES.getKey());
+        CORE_ATTRIBUTES.add(ThreatMetaData.Attribute.REFERENCED_OTHER.getKey());
     }
 
     /**
