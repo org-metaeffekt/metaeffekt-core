02ff4411332d4b4b76d44a1cc722067df0af0212
Karsten Klein <karsten.klein@metaeffekt.com>
2026-09-23 15:13:29 +0200

    AE-2512 Prepare 0.157.2 RELEASE

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,7 +1,15 @@
 echo BUILDING RELEASE version: $1, jira: $2
 set -e
 
-cd metaeffekt-core
+if [ -z "$1" ]; then
+  echo "Missing parameter: Provide the version of the release as first parameter."
+  exit -1
+fi
+
+if [ -z "$2" ]; then
+  echo "Missing parameter: Provide the JIRA issue for the release as third parameter."
+  exit -1
+fi
 
 # update version; version_last points to latest patch or latest minor.patch release
 mvn pre-clean -Pupdate-versions,dev -Dae.core.version=$1 -N -Dae.core.version_last=0.157.1
@@ -16,11 +24,11 @@ rm -rf /Users/kklein/.m2/repository/org/metaeffekt/core
 # run deployment to staging area
 mvn clean deploy -Pdeploy
 
-# only when the build is successfull create the tag
+# only when the build is successful create the tag
 git tag -a $1 -m "$2 $1 RELEASE" || true
 
 echo BUILDING NEW SNAPSHOTS
-mvn pre-clean -P update-versions -Dae.core.version=HEAD-SNAPSHOT -N -Dae.core.version_last=$1
+mvn pre-clean -P update-versions -Dae.core.version=0.157-SNAPSHOT -N -Dae.core.version_last=$1
 mvn clean install -P dev -DskipTests
 
 # commit version update (and other changes)
